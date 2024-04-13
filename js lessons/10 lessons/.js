77fedee744e19f51551ebcb937988c3c045387ea
rostyslaw2 <rostyk.shvets@gmail.com>
2024-04-13 19:36:18 +0300

    Add new file

---
+++ js lessons/10 lessons/.js
@@ -0,0 +1,64 @@
+let words = ["apple", "Car", "house", "Pear", "tues"];
+words.sort((a, b) => a.localeCompare(b)); //сортує по ругістру(алфавіту)
+console.log(words);
+
+//
+
+
+
+const  users5  = require('./base4');
+
+for (x of users5) {
+    console.log(`${x.name}` + " " + `${x.email}`);
+}
+
+
+//
+let children2 = [];
+
+for (x of users5) {
+    children2.push(x.children)
+   // console.log(`${x.children}`)
+       //children2.sort((a, b) => a.localeCompare(b));
+      // console.log(children2);
+}
+
+console.log(children2);
+
+//
+
+let oponent  =  []
+
+const fs = require('fs');
+
+
+function sortChildren(arrayObject) {
+    let sortedChildren = [];
+    for (const word of arrayObject) {
+      sortedChildren = sortedChildren.concat(word.children);
+    }
+    sortedChildren.sort((a, b) => a.localeCompare(b));
+    return sortedChildren;
+  }
+
+  console.log(sortChildren(users5))
+
+  //let result = sortChildren(users5)
+
+  //fs.writeFile('oponent.txt', sortChildren(user5).join('\n'), err => {
+ //   if (err) {
+ //       console.error('Помилка запису у файл:', err);
+ //       return;
+ //   }
+ //   console.log(result);
+//});
+  
+
+
+  let result = sortChildren(users5);
+fs.writeFile("output.txt", JSON.stringify(result), (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("file created");
+  });
---
+++ js lessons/10 lessons/base4.js
@@ -0,0 +1,8 @@
+// users.js
+const users5 = [
+    { id: 1, name: 'Іван', email: 'ivan@example.com', children: ['Олена', 'Павло'] },
+    { id: 2, name: 'Марія', email: 'maria@example.com', children: ['Андрій', 'Катерина', 'Василь'] },
+    { id: 3, name: 'Петро', email: 'petro@example.com', children: ['Марта'] }
+  ];
+  
+  module.exports = users5;
